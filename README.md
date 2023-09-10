dcd27f893bf9013bd2eb7628f0f6351c07f821c4
saintmarrow <144632257+saintmarrow@users.noreply.github.com>
2023-09-10 16:53:02 -0700

    changed from csv to json
    json req'd for easy parsing in javscript for website portion (oops)

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # nonbiblical-vocabulary
-A simple parser script and accompanying webpage that answers whether or not "none of these words are in the bible"
+A simple parser script and accompanying webpage that answers whether or not "none of these words are in the bible".
+
+copies of the bible are courtesy of [OpenBible](https://openbible.com)
--- bibleparser.py
+++ bibleparser.py
@@ -1,36 +1,36 @@
-import re, csv
+import re, json
 # instantiate empty dictionary for the file we're parsing
 dictionary = dict()
 # open the unparsed version of the file
-unparsed_file = open('./unparsed/kjv.txt', 'r')
-# open and erase a file for the parsed version of the dataset
-with open('./parsed/kjv.csv', 'w', newline='') as file:
-    writer =  csv.writer(file)
-    
-    # extract lines from the unparsed file into memory
-    lines = unparsed_file.readlines()
+unparsed_file = open('./unparsed/kjv.txt', 'r', encoding='utf-8-sig')
 
-    # iterate over lines
-    for line in lines:
-        # extract and store the chapter/verse reference
-        chapter_verse = line.split('\t')[0]
-        # remove the chapter/verse reference from the line
-        text = line.split('\t')[1]
-        # remove all special characters, force all letters to lowercase, split into an array of these processed words
-        no_spc_text = re.sub(r"[^a-zA-Z0-9]+", ' ', text).lower().split()
-        # iterate over each word in the array of processed words, storing the word's index in the line
-        for index, word in enumerate(no_spc_text):
-            # check if the word is already in the dictionary
-            if word not in dictionary:
-                # if not, add it to the dictionary as a key
-                # it's value is an array that stores the chapter/verse reference, and the word's index in that verse's line
-                dictionary.update({word:(chapter_verse, index)})
-                # print it for our benefit
-                #print(word, ",", chapter_verse, ",", index)
-                # write it to the target text file
-                # encase the chapter/verse reference in quotes for easy parsing as a csv
-                writer.writerow([word, chapter_verse, index])
-# end of writing, parsed file will automatically close
+# extract lines from the unparsed file into memory
+lines = unparsed_file.readlines()
 
+# iterate over lines
+for line in lines:
+    # extract and store the chapter/verse reference
+    chapter_verse = line.split('\t')[0]
+    # remove the chapter/verse reference from the line
+    text = line.split('\t')[1]
+    # remove all special characters, force all letters to lowercase, split into an array of these processed words
+    no_spc_text = re.sub(r"[^a-zA-Z0-9]+", ' ', text).lower().split()
+    # iterate over each word in the array of processed words, storing the word's index in the line
+    for index, word in enumerate(no_spc_text):
+        # check if the word is already in the dictionary
+        if word not in dictionary:
+            # if not, add it to the dictionary as a key
+            # it's value is an array that stores the chapter/verse reference, and the word's index in that verse's line
+            dictionary.update({word:(chapter_verse, index)})
+        # skip words already in dictionary
+    # end of line, move to next line
+# all lines complete, file parsing complete
 # close the unparsed file
 unparsed_file.close()
+
+# open the parsed file, erasing all previous contents
+with open('./parsed/kjv.json', 'w') as outfile:
+    # dump contents of dictionary to file
+    json.dump(dictionary, outfile, ensure_ascii=False)
+# file will automatically close
+# complete
