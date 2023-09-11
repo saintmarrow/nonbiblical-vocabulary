03265af4d3217e77e06cd1cd0fcdd161e11cde8a
saintmarrow <144632257+saintmarrow@users.noreply.github.com>
2023-09-10 22:12:25 -0700

    completed MVP website
    altered parser to now only store line numbers
    remove posessives from initial parsed dataset
    minimum viable product website completed

--- bibleparser.py
+++ bibleparser.py
@@ -1,6 +1,7 @@
 import re, json
 # instantiate empty dictionary for the file we're parsing
 dictionary = dict()
+#
 # open the unparsed version of the file
 unparsed_file = open('./unparsed/kjv.txt', 'r', encoding='utf-8-sig')
 
@@ -8,20 +9,18 @@ unparsed_file = open('./unparsed/kjv.txt', 'r', encoding='utf-8-sig')
 lines = unparsed_file.readlines()
 
 # iterate over lines
-for line in lines:
-    # extract and store the chapter/verse reference
-    chapter_verse = line.split('\t')[0]
+for line_idx, line in enumerate(lines):
     # remove the chapter/verse reference from the line
     text = line.split('\t')[1]
     # remove all special characters, force all letters to lowercase, split into an array of these processed words
-    no_spc_text = re.sub(r"[^a-zA-Z0-9]+", ' ', text).lower().split()
-    # iterate over each word in the array of processed words, storing the word's index in the line
-    for index, word in enumerate(no_spc_text):
+    no_spc_text = re.sub(r"('[sS])|[^a-zA-Z]+", ' ', text).lower().split()
+    # iterate over each word in the array of processed words, storing the word's first found verse
+    for word_idx, word in enumerate(no_spc_text):
         # check if the word is already in the dictionary
         if word not in dictionary:
             # if not, add it to the dictionary as a key
-            # it's value is an array that stores the chapter/verse reference, and the word's index in that verse's line
-            dictionary.update({word:(chapter_verse, index)})
+            # it's value is an array that stores the chapter/verse reference
+            dictionary.update({word:line_idx})
         # skip words already in dictionary
     # end of line, move to next line
 # all lines complete, file parsing complete
@@ -31,7 +30,10 @@ unparsed_file.close()
 # open the parsed file, erasing all previous contents
 with open('./parsed/kjv.js', 'w') as outfile:
     # dump contents of dictionary to file
-    outfile.write("var KJV = ")
+    outfile.write("var KJV_dict = ")
     json.dump(dictionary, outfile, ensure_ascii=False)
+    outfile.write (";\n var KJV_text = ")
+    json.dump(lines, outfile)
+
 # file will automatically close
 # complete
---
+++ scripts.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+    $("#entry-form").submit(function(e){
+        e.preventDefault(); // this will prevent the submit
+        // load the selected translation
+        // let's just stick with mandatory KJV for now
+        const dict = KJV_dict;
+        // fetch the textarea's contents
+        var contents = $("#entry-field").val();
+        // split up words from any non-alphanumeric characters
+        var word_arr = contents.split(/([^A-Za-z]+)/);
+        console.log(word_arr);
+        // iterate over each word in the text block
+        for (let word_idx in word_arr) {
+            var word = word_arr[word_idx];
+            // check if the word is alphabetical characters
+            if (/^[A-Za-z]+$/.test(word)) {
+                // search word in dictionary
+                if (dict[word] != undefined) {
+                    // word found
+                    // highlight green
+                    word_arr[word_idx] = "<span class=\"tooltip highlight-found\">".concat(word_arr[word_idx]);
+                    // isolate the reference of the verse
+                    var tooltip_title = "<b>" + KJV_text[dict[word]].split("\t")[0] + "</b><hr>";
+                    // isolate the text of the verse
+                    var tooltip_text = KJV_text[dict[word]].split("\t")[1];
+                    // split the text into words and non-words, so we can accurately highlight the target word, regardless of punctuation
+                    // we're not using lookaheads/lookbehinds for things like parentheses or punctiation, because not all browsers support lookbehinds
+                    // so instead we're doing this like cavemen
+                    var split_text = tooltip_text.split(/([^A-Za-z]+)/);
+                    // now iterate over all that shit until we find our target word
+                    for (let tooltip_idx in split_text) {
+                        if (split_text[tooltip_idx].toLowerCase() === word) {
+                            // highlight word in reference tooltip 
+                            split_text[tooltip_idx] = split_text[tooltip_idx].concat("</span>");
+                            split_text[tooltip_idx] = "<span class=\"highlight-found\">".concat(split_text[tooltip_idx]);
+                            // only need the first instance, break out of the loop
+                            break;
+                        }
+                    }
+                    // reassemble the verse text
+                    tooltip_text = split_text.join("");
+                    // now cram that all into a tooltip that's going to be shown as a hover-box over the target word
+                    word_arr[word_idx] = word_arr[word_idx].concat("<span class=\"tooltiptext\">" + tooltip_title + tooltip_text +"</span></span>");
+                } else {
+                    // double check if this is the trailing s from an 's, split() shows weird behavior when I try to include that with our regex
+                    console.log(word)
+                    if (/^[sS]$/.test(word) == false) {
+                        // word not found
+                        // highlight red
+                        word_arr[word_idx] = word_arr[word_idx].concat("</span>");
+                        word_arr[word_idx] = "<span class=\"highlight-notfound\">".concat(word_arr[word_idx]);
+                    }
+                }
+            } else {
+              word_arr[word_idx] = word.replace(/\n\r?/g, "<br/>");  
+            }
+        }
+        // dump new, highlighted text to results box
+        var output = word_arr.join("");
+        $('#processed-block').html(output);         
+    });
+});
+
